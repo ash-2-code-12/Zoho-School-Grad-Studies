19d58ae519fa8fb32c8627073bc8f76e7a4b9559
Ashwin Shankar <128391295+ash-2-code-12@users.noreply.github.com>
2024-07-11 18:09:53 +0530

    Add files via upload

---
+++ Trainer Central Assignments/Java_2024.2/TypeConversions.java
@@ -0,0 +1,8 @@
+// 2. Write a program to demonstrate compatible type conversions. For eg., float to int, double to float, int to short
+
+public class TypeConversions {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+    
+}
