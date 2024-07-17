6c16e5e6968b6fd286922cafc50551b2423af025
Ashwin Shankar <128391295+ash-2-code-12@users.noreply.github.com>
2024-07-17 12:19:15 +0530

    Update ExploreClassFiles.java

--- Trainer Central Assignments/Java Assignments/Assignment-1/ExploreClassFiles.java
+++ Trainer Central Assignments/Java Assignments/Assignment-1/ExploreClassFiles.java
@@ -1,10 +1,6 @@
 
 // Refer read me file for visualising
-public class ExploreClassFiles {
-    public static void main(String[] args) {
-        System.out.println("Upon Compilation there is a '.class' file created for every class present in that file. ");
-    }
-}
+
 class Human{
 
 }
