081bc576ab237abf318ba463d1782e2597aa0da6
Ashwin Shankar <128391295+ash-2-code-12@users.noreply.github.com>
2024-08-01 09:33:56 +0530

    Delete Trainer Central Assignments/Java Assignments directory

--- Trainer Central Assignments/Java Assignments/Assignment-1/Hello_World.java
+++
@@ -1,8 +0,0 @@
-class Hello_world {
-    public static void main(String[] args) {
-        System.out.println(getGreeting());
-    }
-    public static String getGreeting() {
-        return "Hello, World!";
-    }
-}
